1431373d3a9142e555c316d790bc46d56936f48f
SudhaChandrikaY <sudhachandrikay@gmail.com>
2025-02-04 01:24:04 -0600

    Test3

--- app/layout.js
+++ app/layout.js
@@ -5,10 +5,10 @@ import { Toaster } from "@/components/ui/toaster";
 
 const ubuntu = Ubuntu({ weight: "400", subsets: ["latin"] });
 
-export const metadata = {
-  title: "Budget Tracker",
-  description: "Track your budget with ease",
-};
+// export const metadata = {
+//   title: "Budget Tracker",
+//   description: "Track your budget with ease",
+// };
 
 export default function RootLayout({ children }) {
   return (
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
           <title>Budget Tracker</title>
           {/* Favicon Icon */}
           <link rel="icon" type="image/svg+xml" href="/logo.svg" />
-          <meta name="description" content="Track your budget with ease" />
+          <meta title="Budget Tracker" name="description" content="Track your budget with ease" />
         </head>
         <body className={ubuntu.className}>
           <Toaster />
